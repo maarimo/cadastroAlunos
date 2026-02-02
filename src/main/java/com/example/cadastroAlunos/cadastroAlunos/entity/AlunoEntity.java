b09c13469d29814a923b4bfb23eee46745283a43
Matheus Amorim <abmm1234@hotmail.com>
2026-02-02 05:24:23 -0300

    create AlunoEntity

---
+++ src/main/java/com/example/cadastroAlunos/cadastroAlunos/entity/AlunoEntity.java
@@ -0,0 +1,60 @@
+package com.example.cadastroAlunos.cadastroAlunos.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alunos")
+public class AlunoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private int idade;
+
+    public AlunoEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "AlunoEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
