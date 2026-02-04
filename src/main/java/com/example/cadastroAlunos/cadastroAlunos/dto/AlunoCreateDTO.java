19f774bbc723051b6bf1ef8184bcbdef1446aa9d
Matheus Amorim <abmm1234@hotmail.com>
2026-02-04 05:19:02 -0300

    added validation

--- src/main/java/com/example/cadastroAlunos/cadastroAlunos/dto/AlunoCreateDTO.java
+++ src/main/java/com/example/cadastroAlunos/cadastroAlunos/dto/AlunoCreateDTO.java
@@ -1,8 +1,18 @@
 package com.example.cadastroAlunos.cadastroAlunos.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class AlunoCreateDTO {
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @Min(value = 1, message = "Idade deve ser maior que zero")
     private Integer idade;
 
     public AlunoCreateDTO() {
